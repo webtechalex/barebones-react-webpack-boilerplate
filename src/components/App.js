4e3a697804197cc41b4e7432eab951cae407fc9b
Alex Brown <alexanderbrown.info@gmail.com>
2018-01-14 09:33:16 +0000

    add initial content files

---
+++ src/components/App.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const App = () => {
+  return (<h1>Simple bare-bones react config!</h1>)
+}
+
+export default App
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,29 @@
-const path = require('path')
-
 module.exports = {
   entry: __dirname + '/src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: __dirname + '/dist',
     filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: '/node_modules/',
+        use: [
+          {loader: 'babel-loader'}
+        ]
+      },
+      {
+        test: /\.css$/,
+        exclude: '/node_modules/',
+        use: [
+          {loader: 'style-loader'},
+          {loader: 'css-loader'}
+        ]
+      }
+    ]
   }
 }
